b87cd8fa8009eb6bbff9a06cdc8cb0e38ad644bb
Kurt Shumaker <kshumak@gmail.com>
2018-09-13 20:53:53 -0700

    Removed additional methods
    Removed duplicate static versions of methods from Controller class.
    
    Made Controller class a singleton.

--- src/Controller.ts
+++ src/Controller.ts
@@ -7,29 +7,19 @@ export class Controller{
     private firstPoint : Point;
     
     constructor(canvas : HTMLCanvasElement, view : View) {
-        canvas.addEventListener('mousemove',Controller.staticOnMouseMove);
-        canvas.addEventListener('click', Controller.staticOnMouseClick);
-        canvas.addEventListener('touchstart', Controller.staticOnScreenTouch);
-        canvas.addEventListener('touchmove', Controller.staticOnTouchMove);
-        Controller.instance = this;
-        this.view = view;
+        if (!Controller.instance) {
+            Controller.instance = this;
+            this.view = view;
+            canvas.addEventListener('mousemove', (event) => this.onMouseMove(event));
+            canvas.addEventListener('click', (event) => this.onMouseClick(event));
+            canvas.addEventListener('touchstart', (event) => this.onScreenTouch(event));
+            canvas.addEventListener('touchmove', (event) => this.onTouchMove(event));
+        }
+        else
+            console.warn("Controller already exists. An attempt to create another instance of Controller was made.");
     }
 
-    static staticOnMouseMove(event : MouseEvent) {
-        Controller.instance.onMouseMove(event);
-    }
-    
-    static staticOnMouseClick(event : MouseEvent) {
-        Controller.instance.onMouseClick(event);
-    }
 
-    static staticOnScreenTouch(event : TouchEvent) {
-        //Controller.instance.onScreenTouch(event);
-    }
-
-    static staticOnTouchMove(event : TouchEvent) {
-       //Controller.instance.onTouchMove(event);
-    }
 
     onMouseClick(event : MouseEvent){
         this.view.click(new Point(event.offsetX, event.offsetY));
@@ -37,6 +27,14 @@ export class Controller{
 
     onMouseMove(event : MouseEvent) {
         this.view.cursorMove(new Point(event.offsetX, event.offsetY));
+    }
+
+    onScreenTouch(event : TouchEvent) {
+        //handle screen touch event;
+    }
+
+    onTouchMove(event : TouchEvent) {
+       // handle screen touch movement event;
     }
 
 }
