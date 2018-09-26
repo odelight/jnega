bc72959c229a26566f68bc0917fa472a166693b7
Olof <olofdelight@gmail.com>
2018-09-25 17:59:59 -0700

    More misc. cleanup

--- src/Math/Paths.ts
+++
@@ -1,6 +0,0 @@
-import { Point } from "../Model/Point";
-import { Vector } from "./Vector";
-
-export function basicCosineWave(center : Point, direction : Vector, period : number) : (t : number) => Point {
-    return (t) => (center.offsetBy(direction.scalarProduct(Math.cos(t*(2*Math.PI/period)))));
-}
